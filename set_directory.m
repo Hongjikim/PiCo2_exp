39d3abf5c056a7e93ccc5210bc7dfddc08bac238
Dongju Park <dongjupark@Dongjus-MacBook-Pro.local>
2020-06-24 10:22:41 +0900

    add dj's directory

--- set_directory.m
+++ set_directory.m
@@ -17,7 +17,7 @@ switch where
         basedir = '/Users/hongji/Dropbox/github_hongji/PiCo2_exp';
         
     case 'dj_mac'
-        % basedir = '/Users/hyebinkim/Dropbox/Cocoan lab/7T HCP/stimuli_candidates/git_7T_hcp_emotion';
+        basedir = '/Users/dongjupark/Dropbox/Cocoan_github/PiCo2_exp';
         
     case 'WL01'
         basedir = 'C:\Users\Cocoanlab_WL01\Desktop\PiCo_exp-master';
