3073975462256e5df7fd7312eb1087e9a6dfdf34
Hongji Kim <redkim94@hanmail.net>
2020-07-08 14:58:44 +0900

    updates

--- pico_wholewords.m
+++ pico_wholewords.m
@@ -6,13 +6,13 @@ subject_dir = filenames(fullfile(datdir, [sid '*']), 'char');
 [~, sid] = fileparts(subject_dir);
 sid = sid(1:7);
 
-dat_file{1} = fullfile(subject_dir, ['THOUGHT_SAMPLING_' sid '_run1.mat']);
-dat_file{2} = fullfile(subject_dir, ['THOUGHT_SAMPLING_' sid '_run2.mat']);
-dat_file{3} = fullfile(subject_dir, ['THOUGHT_SAMPLING_' sid '_run3.mat']);
+for rr = 1:size(filenames(fullfile(subject_dir, 'THOUGHT*mat')),1)
+    dat_file{rr} = fullfile(subject_dir, ['THOUGHT_SAMPLING_' sid '_run', num2str(rr), '.mat']);
+end
 
-data = cell(3, 15);
+data = cell(size(filenames(fullfile(subject_dir, 'THOUGHT*mat')),1), 15);
 %
-for i=1:3
+for i=1:size(filenames(fullfile(subject_dir, 'THOUGHT*mat')),1)
     load(dat_file{i});
     data(i,:)=response;
 end
