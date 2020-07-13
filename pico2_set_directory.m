9eb267212ed211f40cf71e669d19c8487a368c36
Hongji Kim <redkim94@hanmail.net>
2020-07-13 16:35:29 +0900

    updates

--- pico2_set_directory.m
+++ pico2_set_directory.m
@@ -1,4 +1,4 @@
-function basedir = set_directory(where)
+function basedir = pico2_set_directory(where)
 %
 % It sets up the essential directories according to where you are.
 % and in case of server, it adds paths
@@ -17,10 +17,10 @@ switch where
         basedir = '/Users/hongji/Dropbox/PiCo2_sync/PiCo2_exp';
         
     case 'dj_mac'
-        basedir = '/Users/dongjupark/Dropbox/PiCo2/PiCo2_exp-master';
+        basedir = '/Users/dongjupark/Dropbox/PiCo2/PiCo2_exp';
         
     case 'WL01'
-        basedir = 'C:\Users\Cocoanlab_WL01\Dropbox\PiCo2\PiCo2_exp-master';
+        basedir = 'C:\Users\Cocoanlab_WL01\Dropbox\PiCo2\PiCo2_exp';
 end
 
 addpath(genpath(basedir));
--- run_pico2_fmri_main.m
+++ run_pico2_fmri_main.m
@@ -3,19 +3,18 @@
 %% FREE THINKING (N = 1, 2, 3)
 
 clear;
-
-basedir = set_directory('hj_mac'); % 'dj_mac', 'WL01'
+basedir = pico2_set_directory('dj_mac'); % 'dj_mac', 'WL01'
 cd(basedir);
 
 %%
-pico_fmri_resting(basedir);% 'test', 'biopac'
+pico2_fmri_resting(basedir);% 'test', 'biopac'
 
 %% INPUT WORDS (N = 1, 2, 3)
 
 cd(basedir);
-pico_wordsampling(basedir);     
+pico2_wordsampling(basedir);     
 
 %% WORD SURVEY
 cd(basedir);
-[words, sid] = pico_wholewords(basedir);
+[words, sid] = pico2_wholewords(basedir);
 a_fast_fmri_survey(basedir, sid, words); % body map right boundary
