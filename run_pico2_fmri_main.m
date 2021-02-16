2319cfe8a0424bec8c0c04405800d871d31146ea
Hongji Kim <redkim94@hanmail.net>
2021-02-16 21:30:28 +0900

    updates

--- run_pico2_fmri_main.m
+++ run_pico2_fmri_main.m
@@ -1,6 +1,6 @@
 %% Set directory and register participant 
 clear;
-[basedir, sid, subject_dir] = pico2_directory_sub_info('exp_room'); 
+[basedir, sid, subject_dir] = pico2_directory_sub_info('WL01'); 
 % 'exp_room', 'dj_mac', 'WL01', 'BE_imac' 'int01', 'hj_mac','hm_mac', 'je_mac'
 cd(basedir);
 
@@ -44,3 +44,4 @@ survey = pico2_post_type02_word_survey(basedir, sid, words); % ,'mgkey');
 cd(basedir);  
 words = pico2_wholewords(basedir, sid); 
 pico2_post_type03_fast_word_survey(basedir, sid(1:7), words); %, 'mgkey'); 
+qq
